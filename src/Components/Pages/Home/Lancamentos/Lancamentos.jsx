07a5bfeb62c2df22a685393a473dd0634b284993
Deivison Gonçalves <deivisoncpmg201@gmail.com>
2024-01-07 01:34:21 -0300

    remoção de itens inutilizados eadição de responsividade
    remoção de itens inutilizados e adição da responsividade no card lançamento

--- src/Components/Pages/Home/Lancamentos/Lancamentos.jsx
+++ src/Components/Pages/Home/Lancamentos/Lancamentos.jsx
@@ -10,14 +10,14 @@ function Lancamentos(){
             <div className={style.container}>
                 <div className={style.text}>
                     <div className={style.title}>
-                        <span class="material-symbols-outlined">rocket_launch</span>
+                        <span className="material-symbols-outlined">rocket_launch</span>
                         <h1>Lançamentos</h1>
                     </div>
                     <div className={style.subtitle}>
                     <p>Últimos Lançamentos <strong>pra você</strong></p>
                     </div>
                 </div>
-                <div className={`${style.cardsLancamentos} dragscroll` }>
+                <div className={style.cardsLancamentos}>
                     <CardLancamentos />
                     <CardLancamentos />
                     <CardLancamentos />
